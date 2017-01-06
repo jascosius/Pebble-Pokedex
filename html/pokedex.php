2134ce4d72c5fd17e291c5213bbea042817bbb8a
Marius Rasch <git@marius-rasch.de>
2017-01-06 17:53:50 +0100

    Added jQuery and jQuery Mobile for local use; Fixes bug when selecting second language

--- html/pokedex.php
+++ html/pokedex.php
@@ -4,9 +4,9 @@
     <title>Pokedex</title>
     <meta charset='utf-8'>
     <meta name='viewport' content='width=device-width, initial-scale=1'>
-    <link rel='stylesheet' href='http://code.jquery.com/mobile/1.3.2/jquery.mobile-1.3.2.min.css' />
-    <script src='http://code.jquery.com/jquery-1.9.1.min.js'></script>
-    <script src='http://code.jquery.com/mobile/1.3.2/jquery.mobile-1.3.2.min.js'></script>
+    <link rel='stylesheet' href='jquery.mobile-1.4.5.min.css' />
+    <script src='jquery-1.11.3.min.js'></script>
+    <script src='jquery.mobile-1.4.5.min.js'></script>
     <style>
       .ui-header .ui-title { margin-left: 1em; margin-right: 1em; text-overflow: clip; }
     </style>
@@ -69,21 +69,21 @@
               $second_lang = $_GET['second_lang'];
             }
 
-            $lang_eng = "";
-            $lang_ger = "";
-            $lang_none = "";
+            $lang_eng2 = "";
+            $lang_ger2 = "";
+            $lang_none2 = "";
 	
             if ($second_lang == 0) {
-              $lang_eng = " checked";
+              $lang_eng2 = " checked";
             } elseif ($second_lang == 1) {
-              $lang_ger = " checked";
+              $lang_ger2 = " checked";
             } else {
-              $lang_none = " checked";
+              $lang_none2 = " checked";
             }
 	
-            echo '<input id="second_lang_eng" name="second_lang" value="0" data-theme="" type="radio"' . $lang_eng . $lang_eng_disabled . '><label for="second_lang_eng">English</label>';
-            echo '<input id="second_lang_ger" name="second_lang" value="1" data-theme="" type="radio"' . $lang_ger . $lang_ger_disabled . '><label for="second_lang_ger">German</label>';
-            echo '<input id="second_lang_none" name="second_lang" value="-1" data-theme="" type="radio"' . $lang_none . '><label for="second_lang_none">None</label>';
+            echo '<input id="second_lang_eng" name="second_lang" value="0" data-theme="" type="radio"' . $lang_eng2 . '><label for="second_lang_eng">English</label>';
+            echo '<input id="second_lang_ger" name="second_lang" value="1" data-theme="" type="radio"' . $lang_ger2 . '><label for="second_lang_ger">German</label>';
+            echo '<input id="second_lang_none" name="second_lang" value="-1" data-theme="" type="radio"' . $lang_none2 . '><label for="second_lang_none">None</label>';
 	      ?>
         </fieldset>
       </div>
@@ -93,19 +93,19 @@
       function handleClick(myRadio) {
         if(myRadio.id == 'first_lang_eng') {
           $('#second_lang_ger').prop('disabled', false);
-          $('#second_lang_eng').prop('disabled', true);
           if($('#second_lang_eng').is(':checked')) {
             $('#second_lang_eng').prop('checked', false);
             $('#second_lang_ger').prop('checked', true);
           }
+          $('#second_lang_eng').prop('disabled', true);
         }
         if(myRadio.id == 'first_lang_ger') {
           $('#second_lang_eng').prop('disabled', false);
-          $('#second_lang_ger').prop('disabled', true);
           if($('#second_lang_ger').is(':checked')) {
             $('#second_lang_ger').prop('checked', false);
             $('#second_lang_eng').prop('checked', true);
           }
+          $('#second_lang_ger').prop('disabled', true);
         }
         $("[type=checkbox]").checkboxradio();
         $("[data-role=controlgroup]").controlgroup("refresh");
@@ -120,6 +120,15 @@
       }
 
       $().ready(function() {
+        console.log("Hallo");
+        if($('#first_lang_eng').is(':checked')) {
+          $('#second_lang_eng').prop('disabled', true);
+        }
+        if($('#first_lang_ger').is(':checked')) {
+          $('#second_lang_ger').prop('disabled', true);
+        }
+        $("[type=checkbox]").checkboxradio();
+        $("[data-role=controlgroup]").controlgroup("refresh");
         $("#cancel").click(function() {
           console.log("Cancel");
           document.location = "pebblejs://close#";
